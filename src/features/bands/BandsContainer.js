cc1aa2d0fe00381ff5475e885da5a650503333a7
AhmadSaman <ahmad.00079821@gmail.com>
2021-08-30 15:08:56 +0300

    commit tree 82b7c753496ce48d8af52c9905c2f834e8794791

--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -2,25 +2,25 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import BandInput from "./BandInput";
 import { bandAdded } from "./bandsSlice";
-
+import Band from "./Band";
 function BandsContainer() {
-  const dispatch = useDispatch();
-  const bands = useSelector((state) => state.bands.entities);
+	const dispatch = useDispatch();
+	const bands = useSelector((state) => state.bands.entities);
 
-  function handleBandSubmit(name) {
-    dispatch(bandAdded(name));
-  }
+	function handleBandSubmit(name) {
+		dispatch(bandAdded(name));
+	}
 
-  return (
-    <div>
-      <BandInput onBandSubmit={handleBandSubmit} />
-      <ul>
-        {bands.map((band) => (
-          <li key={band}>{band}</li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<BandInput onBandSubmit={handleBandSubmit} />
+			<ul>
+				{bands.map((band) => (
+					<Band key={band} band={band} />
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default BandsContainer;
